4159282d23c845806ae24ae06c753589fd53b9cd
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2024-12-12 14:03:01 +0300

    Add documentation for RC4 class and its methods, including encrypt and decrypt functionalities

--- src/main/kotlin/one/wabbit/rc4/RC4.kt
+++ src/main/kotlin/one/wabbit/rc4/RC4.kt
@@ -2,13 +2,30 @@ package one.wabbit.rc4
 
 import kotlin.experimental.xor
 
+/**
+ * The RC4 class is an implementation of the RC4 stream cipher algorithm.
+ *
+ * @property S A 256-byte state array used in the RC4 algorithm. Must be initialized with exactly 256 bytes.
+ */
 class RC4(val S: ByteArray) {
     init {
         require(S.size == 256) { "S must be 256 bytes" }
     }
 
+    /**
+     * Creates a copy of the current RC4 instance, duplicating its state array (S).
+     *
+     * @return A new RC4 instance with an identical state array to the original.
+     */
     fun copy(): RC4 = RC4(S.copyOf())
 
+    /**
+     * Encrypts the given plaintext using the RC4 stream cipher algorithm.
+     * This implementation modifies the internal state of the RC4 object during encryption.
+     *
+     * @param plaintext The input byte array to be encrypted.
+     * @return A byte array containing the encrypted ciphertext.
+     */
     fun encrypt(plaintext: ByteArray): ByteArray {
         val ciphertext = ByteArray(plaintext.size)
         var i = 0
@@ -26,11 +43,26 @@ class RC4(val S: ByteArray) {
         return ciphertext
     }
 
+    /**
+     * Decrypts the given ciphertext using the RC4 stream cipher algorithm.
+     * This operation is typically the reverse of encryption, but in RC4,
+     * encryption and decryption processes are identical.
+     *
+     * @param ciphertext The input byte array to be decrypted.
+     * @return A byte array containing the decrypted plaintext.
+     */
     fun decrypt(ciphertext: ByteArray): ByteArray {
         return encrypt(ciphertext)
     }
 
     companion object {
+        /**
+         * Initializes an RC4 cipher instance using the provided key.
+         *
+         * @param key A byte array representing the key. The key size must be between 1 and 256 bytes.
+         * @return An instance of the RC4 cipher with the initialized state array.
+         * @throws IllegalArgumentException If the key size is not within the allowed range (1 to 256 bytes).
+         */
         fun fromKey(key: ByteArray): RC4 {
             require(!(key.size < 1 || key.size > 256)) { "key must be between 1 and 256 bytes" }
 
